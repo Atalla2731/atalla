8b9c50d59df434f2a06b85d3cc8893be18fea845
Atalla2731 <d.atalla1610@gmail.com>
2025-05-27 08:17:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("selamat datang di situs ini)
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "mari mengasah otak di situs ini"
 )
 st.image("image.jpg",width=2000)
