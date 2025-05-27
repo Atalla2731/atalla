e8f66767872e63a7276e9712813fe8b0bf484797
Atalla2731 <d.atalla1610@gmail.com>
2025-05-27 08:33:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,12 @@ st.write(
     "mari mengasah otak di situs ini"
 )
 st.image("image.jpg",width=2000)
+
+st.title("situs halal")
+st.header("Aplikasi Game mengasah otak")
+angka = st.number_input("tulis sebuah angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah bilangan genap")
+else:
+  st.write(f"{angka} adalah bilangan ganjil")
